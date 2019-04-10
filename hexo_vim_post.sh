d6da48976c60b648a0fad1c5dc3bddeac8e7f3ad
REN YongXiang <riantr@gmail.com>
2019-04-11 01:07:54 +0800

    Voila!

--- hexo_vim_post.sh
+++ hexo_vim_post.sh
@@ -4,8 +4,12 @@ else
 
     if [ ! -f source/_posts/$1.md ];then
         hexo n $1 1>>posts.log
-        sed -i "s/tags:/& $(echo $1|xargs -d-)/" source/_posts/$1.md
-        vim source/_posts/$1.md
+        for i in $(echo $1|xargs -d-|xargs -n 1)
+        do
+            sed -i "s/tags:/&\n  - $i/" source/_posts/$1.md
+        done
+        sed -i "2a\\categories:\n  -" source/_posts/$1.md
+        vim source/_posts/$1.md +4
     else
         vim source/_posts/$1.md
     fi
