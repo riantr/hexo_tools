a45e874d658573c70196c57175e4630067a7b81c
REN YongXiang <riantr@gmail.com>
2019-04-11 00:54:39 +0800

    Voila!

--- hexo_tag_post.sh
+++ hexo_tag_post.sh
@@ -1,8 +1,16 @@
  if [ -z $1 ];then
      echo usage
  else
-     [ -n $5 ] && sed -i "s/tags:/& $5/" source/_posts/$1.md
-     [ -n $4 ] && sed -i "s/tags:/& $4/" source/_posts/$1.md
-     [ -n $3 ] && sed -i "s/tags:/& $3/" source/_posts/$1.md
-     [ -n $2 ] && sed -i "s/tags:/& $2/" source/_posts/$1.md
+     if [ -n $5 ];then
+     sed -i "/tags/a\  - $5" source/_posts/$1.md
+     fi
+     if [ -n $4 ];then
+     sed -i "/tags/a\  - $4" source/_posts/$1.md
+     fi
+     if [ -n $3 ];then
+     sed -i "/tags/a\  - $3" source/_posts/$1.md
+     fi
+     if [ -n $2 ];then
+     sed -i "/tags/a\  - $2" source/_posts/$1.md
+     fi
  fi
